b294e631d69a03dccabe0b4844c8ca2b3e9d1ef3
Alzamna <165247202+Alzamna@users.noreply.github.com>
2024-11-28 21:11:38 +0700

    Add files via upload

---
+++ prediksi_harga.py
@@ -0,0 +1,40 @@
+import pickle
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+model = pickle.load(open("prediksi_harga_mobil.sav", 'rb'))
+
+st.title("Prediksi Harga Mobil")
+
+st.header("Dataset")
+df1 = pd.read_csv("CarPrice.csv")  
+st.dataframe(df1)
+
+st.write("Grafik Highwaympg")
+chart_highwaympg = pd.DataFrame(df1, columns=["highwaympg"])
+st.line_chart(chart_highwaympg)
+
+st.write("Grafik Curbweight")
+chart_curbweight = pd.DataFrame(df1, columns=["curbweight"])
+st.line_chart(chart_curbweight)
+
+st.write("Grafik Horsepower")
+chart_horsepower = pd.DataFrame(df1, columns=["horsepower"])
+st.line_chart(chart_horsepower)
+
+st.write("Masukkan Nilai Variabel Independen:")
+highwaympg = st.number_input("Highway-mpg", min_value=0, max_value=100, value=25)
+curbweight = st.number_input("Curbweight", min_value=0, max_value=5000, value=2500)
+horsepower = st.number_input("Horsepower", min_value=0, max_value=1000, value=150)
+
+if st.button("Prediksi"):
+    try:
+        car_prediction = model.predict([[highwaympg, curbweight, horsepower]])
+        
+        harga_mobil = float(car_prediction[0])
+        harga_mobil_formatted = f"${harga_mobil:,.2f}"  
+        
+        st.success(f"Harga mobil diprediksi: {harga_mobil_formatted}")
+    except Exception as e:
+        st.error(f"Terjadi kesalahan: {e}")
